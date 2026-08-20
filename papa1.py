2ecfe16a4ade5a687e940e17c98d62bec172de53
rilasontiwari <rilasontiwari@gmail.com>
2026-08-20 13:49:28 +0545

    fun

---
+++ papa1.py
@@ -0,0 +1,16 @@
+#input
+age=int(input("enter your age="))
+#input name
+name=input("enter your name=")
+#input marks
+mark1=float(input("enter your math marks="))
+mark2=float(input("enter your computer marks="))
+print(name)
+print(mark1)
+print("your computer mark is=",mark2)
+print("thank you")
+print(age)
+print(age,name,mark1)
+sum=mark1+mark2 
+print(sum)
+
